86e3b93f26e18fc99309ea1b3a8d81990c9a091d
lkbajaj <leynabajaj@umass.edu>
2025-06-14 11:14:44 -0400

    higher quality plate images

--- plate_imager.py
+++ plate_imager.py
@@ -3,12 +3,12 @@ from PIL import Image, ImageDraw, ImageFont
 
 # example create a Swedish plate
 def create_swedish_plate(number):
-    image = Image.open("plate-templates/sweden.png").convert('RGBA')
+    image = Image.open("plate-templates/sweden-hq.png").convert('RGBA')
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("fonts/DINMittelschriftStd.otf",size=95)
+    font = ImageFont.truetype("fonts/DINMittelschriftStd.otf",size=160)
 
-    text = number.replace(" ","   ")
-    position = (85,20)
+    text = number.replace(" ","  ")
+    position = (150,60)
     color = 'black'
 
     draw.text(position, text, font=font, fill=color)
